3560c8b6b52d6b2c0e350e87b03b3212c33ae267
annafaix <annafaix@gmail.com>
2020-07-05 21:27:06 +0200

    massive change in style & html

--- sign-in sing-up form/script.js
+++ sign-in sing-up form/script.js
@@ -1,1 +1,25 @@
 // validate form..
+
+
+// add some animation
+let signInSection = document.getElementById("sign-in-section");
+let signUpSection = document.getElementById("sign-up-section");
+
+let forms = document.getElementsByClassName("forms")[0];
+let signUpForm = document.getElementById("sign-up-form-section");
+let signInForm = document.getElementById("sign-in-form-section")
+
+let overlay = document.getElementsByClassName("overlay")[0];
+let signUpBtnOverlay = document.getElementById("sign-up-btn-overlay");
+let signInBtnOverlay = document.getElementById("sign-in-btn-overlay")
+
+signUpBtnOverlay.addEventListener("click", function() {
+  overlay.classList.add("overlay-transform");
+  forms.classList.add("forms-transform");
+})
+
+signInBtnOverlay.addEventListener("click", function() {
+  overlay.classList.remove("overlay-transform");
+  forms.classList.remove("forms-transform")
+})
+
