d0c8005d2d1ad0984ee91e8050168f78b1a5e760
annafaix <annafaix@gmail.com>
2020-07-17 21:38:41 +0200

    add react-icons/styling/split up code

--- web_shop/src/App.js
+++ web_shop/src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.scss';
 import logo from './default-monochrome-black.svg'
 import products from './products.json';
+import Cart from './Cart';
+import { AiOutlineShoppingCart } from 'react-icons/ai';
+
  
 function Navigation(){
   return (
@@ -10,54 +13,44 @@ function Navigation(){
       <img src={logo} alt="Logo"  className="logo"/>
       <ul className="navigation-menu">
         <li><Link to="/">All Books</Link> </li>
-        <li><Link to="/cart">Cart</Link></li>
+        <li><Link to="/cart">Cart <AiOutlineShoppingCart/></Link></li>
       </ul>
     </nav>
   )
 }
 
-function Cart(){
-  return(
-    <h1>Hello from Cart and company</h1>  
-  )
-}
 
 function ProductList(){
-  const booksItem = products.map((element) => {
+  
+  const booksItems = products.map((element) => {
    return(
-    <a key={element.id}>
-      <li>
-        <img src={element.book_image} alt= { `cover of ${element.title}` } />
-        <div>
-
-        <h2> {element.title}</h2>
-        <h3>{element.author}</h3>
-        <p className="price">{element.price} ‎€</p>
-        </div>
-      </li>
-    </a>)
+     <ProductItem item={element} key={element.id} />
+    )
   });
 
   return(
     <React.Fragment>
-      <h1>Books to read in summer</h1>
       <ul className="bookList">
-        {booksItem}
+        {booksItems}
       </ul>
     </React.Fragment>
   )
 }
 
-function ProductDetail(id){
+function ProductItem(props){
+  let item = props.item;
   return(
     <React.Fragment>
-      <span>Back to list</span>
-      <div>
-        <img src="" alt="cover of">Img</img>
-        <h1>Title</h1>
-        <h2>Author</h2>
-        <p>Description</p>
-      </div>
+     <a >
+      <li>
+        <img src={item.book_image} alt= { `cover of ${item.title}` } />
+        <div>
+          <h2> {item.title}</h2>
+          <h3>{item.author}</h3>
+          <p className="price">{item.price} ‎€ <AiOutlineShoppingCart/></p>
+        </div>
+      </li>
+    </a>
     </React.Fragment>
   )
 }
@@ -65,9 +58,25 @@ function ProductDetail(id){
 function App() {
   let [list, updateList] = useState(products);
 
+ // Why not display products in different order when comoponent will mount? 
+ let shuffleArray = (arr) => {
+  let index = arr.length;
+  let tempValue, randomIndex; 
+  while(index !== 0){
+    // take random number 0-1 and multiply with length of array, then get rid of decimal
+    randomIndex = Math.floor(Math.random() * index);
+    index--;
+    // save temporary number to shuffle
+    tempValue = arr[index];
+    // shuffle two positions
+    arr[index] = arr[randomIndex];
+    arr[randomIndex] = tempValue;
+  }
+}
   useEffect(() =>{
-    updateList(list=products);
-  })
+    updateList(shuffleArray(list)) ;
+  }, [])
+
   return (
     <div className="App">
       <Router>
@@ -76,8 +85,18 @@ function App() {
         </header>
         <main>
           <Switch>
-            <Route path="/cart" component={Cart} />
-            <Route exact path="/" component={ProductList} />
+            <Route path="/cart" >
+              <Cart/>
+            </Route>
+            <Route exact path="/"  >
+              <div className="introduction">
+                <h1>Recommended Books to read in the summer</h1>
+                <p>Here you can find some inspiration for summer reading. These are the books I would like to read in the next months. 
+                  <br/>Choose a book, read more details about it and "add" it to your cart/wish list! Almost like in a real online book store..
+                </p>
+              </div>
+              <ProductList/> 
+            </Route>
           </Switch>
         </main>
       </Router>
---
+++ web_shop/src/Cart.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+
+function Cart(){
+  return(
+    <h1>comming soon...Cart and company</h1>  
+  )
+}
+
+export default Cart;
